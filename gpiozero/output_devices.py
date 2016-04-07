dfe0b3144bdaaee865cacb3c4cb3d1829265745c
Ben Nuttall <ben@bennuttall.com>
2016-04-07 01:05:01 +0100

    Implement proportional value setter and add docstrings

--- gpiozero/output_devices.py
+++ gpiozero/output_devices.py
@@ -824,22 +824,85 @@ class Motor(SourceMixin, CompositeDevice):
 
 
 class Servo(PWMOutputDevice):
-    def __init__(self, pin=None, active_high=True, initial_value=0, frequency=100, max_angle=180, minimum=0.033, maximum=0.254):
-        super(Servo, self).__init__(pin, active_high, initial_value, frequency)
+    """
+    Extends :class:`PWMOutputDevice` and represents a hobby servo, which can be
+    instructed to rotate to a given angle.
+
+    A typical configuration of such a device is to connect it to 5V, GND and a
+    single GPIO pin. The servo is controlled by sending a pulse signal which
+    determines the angle the device should rotate to.
+
+    :param int pin:
+        The GPIO pin which the servo is attached to. See :doc:`notes` for
+        valid pin numbers.
+
+    :param float initial_value:
+        XXX
+
+    :param float initial_angle:
+        XXX
+
+    :param int frequency:
+        The frequency (in Hz) of pulses emitted to drive the LED. Defaults
+        to 100Hz.
+
+    :param int max_angle:
+        The maximum angle the servo can rotate to. Defaults to 180 degrees.
+
+    :param int min_duty_cycle:
+        The duty cycle required to set the servo to a 0 degree angle.
+
+    :param int max_duty_cycle:
+        The duty cycle required to set the servo to its maximum angle.
+
+        max_angle=180, min_duty_cycle=0.033,
+        max_duty_cycle=0.254
+    """
+    def __init__(self, pin=None, initial_value=0, initial_angle=None,
+                frequency=100, max_angle=180, min_duty_cycle=0.033,
+                max_duty_cycle=0.254):
+        active_high = True
         self._max_angle = max_angle
-        self._minimum = minimum
-        self._maximum = maximum
-        self._angle = None  # don't know what it is on init
+        self._min_duty_cycle = min_duty_cycle
+        self._max_duty_cycle = max_duty_cycle
+        super(Servo, self).__init__(pin, active_high, initial_value, frequency)
+
+    @property
+    def value(self):
+        """
+        The proportion of the potential angle the servo is currently positioned.
+        0.0 is 0 degrees, 1.0 is the maximum angle. Values in between may be
+        specified for varying positions proportionally between 0 and the maximum
+        angle.
+        """
+        duty_cycle = self._read()
+        value = (
+            (duty_cycle - self._min_duty_cycle) /
+            (self._max_duty_cycle - self._min_duty_cycle)
+        )
+        return value
+
+    @value.setter
+    def value(self, value):
+        self.angle = value * self._max_angle
 
     @property
     def angle(self):
+        """
+        The angle the servo is currently positioned. Value will be between 0
+        and max_angle.
+        """
         return self._angle
 
     @angle.setter
     def angle(self, value):
-        if 0 > value > self._max_angle:
+        if not 0 <= value <= self._max_angle:
             raise OutputDeviceBadValue(
                 "angle must be between 0 and %s" % self._max_angle
             )
         self._angle = value
-        self.value = ((self._maximum - self._minimum) * value / self._max_angle) + self._minimum
+        duty_cycle = (
+            (self._max_duty_cycle - self._min_duty_cycle)
+            * value / self._max_angle
+        ) + self._min_duty_cycle
+        self._write(duty_cycle)
